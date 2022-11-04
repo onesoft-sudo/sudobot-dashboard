546bb4221681a0ddd08db2ff1d72830483e694f9
Ar Rakin <rakinar2@gmail.com>
2022-11-04 11:15:54 +0000

    feat: footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <div className="bg-[#222] p-5 lg:px-[20%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-            <div className="pr-5 md:order-last">
+        <div className="bg-[#222] px-5 lg:px-[18%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+            <div className="mr-10 pt-2 pb-5 order-last md:order-first">
                 <a href="https://www.onesoftnet.eu.org" target="_blank" rel="noreferrer" className="text-white no-underline text-2xl md:text-3xl">
                     <img src="https://res.cloudinary.com/rakinar2/image/upload/v1667559622/OSN-Logo_bjp4gk.png" height="auto" width="100%" alt="OneSoftNet" />
                 </a>
                 <p>Copyright &copy; OneSoftNet, Inc. 2020-{new Date().getFullYear()} and all contributors.</p>
             </div>
-            <div>
+            <div className="py-5">
                 <h3>Contact</h3>
 
                 <ul className="list-none mt-3">
@@ -18,7 +18,7 @@ export default function Footer() {
                     <li><a target="_blank" rel="noreferrer" href="https://discord.gg/892GWhTzgs">Discord Server</a></li>
                 </ul>
             </div>
-            <div>
+            <div className="py-5">
                 <h3>About</h3>
 
                 <ul className="list-none mt-3">
@@ -28,7 +28,7 @@ export default function Footer() {
                     <li><a target="_blank" rel="noreferrer" href="https://github.com/onesoft-sudo/sudobot/graphs/contributors">Contributors</a></li>
                 </ul>
             </div>
-            <div>
+            <div className="py-5">
                 <h3>Useful Links</h3>
 
                 <ul className="list-none mt-3">
