079b9b6165d2da26570fd3a184cb9b993cbcffbf
Ar Rakin <rakinar2@gmail.com>
2022-11-04 11:11:33 +0000

    feat: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,42 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div className="bg-[#222] p-5">
-            <div className="text-2xl md:text-3xl">OneSoftNet</div>
-            <p>Copyright &copy; OneSoftNet, Inc. 2020-{new Date().getFullYear()}</p>
+        <div className="bg-[#222] p-5 lg:px-[20%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+            <div className="pr-5 md:order-last">
+                <a href="https://www.onesoftnet.eu.org" target="_blank" rel="noreferrer" className="text-white no-underline text-2xl md:text-3xl">
+                    <img src="https://res.cloudinary.com/rakinar2/image/upload/v1667559622/OSN-Logo_bjp4gk.png" height="auto" width="100%" alt="OneSoftNet" />
+                </a>
+                <p>Copyright &copy; OneSoftNet, Inc. 2020-{new Date().getFullYear()} and all contributors.</p>
+            </div>
+            <div>
+                <h3>Contact</h3>
+
+                <ul className="list-none mt-3">
+                    <li><a href="mailto:sudobot@onesoftnet.eu.org">Email</a></li>
+                    <li><a href="mailto:rakinar2@onesoftnet.eu.org">Email (Webmaster)</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://discord.gg/892GWhTzgs">Discord Server</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>About</h3>
+
+                <ul className="list-none mt-3">
+                    <li><Link href="/about">About SudoBot</Link></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://github.com/onesoft-sudo/sudobot">GitHub</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://github.com/onesoft-sudo/sudobot/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D">Report a bug</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://github.com/onesoft-sudo/sudobot/graphs/contributors">Contributors</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Useful Links</h3>
+
+                <ul className="list-none mt-3">
+                    <li><a target="_blank" rel="noreferrer" href="https://docs.onesoftnet.eu.org/legal/terms.md">Terms of Service</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://docs.onesoftnet.eu.org/legal/policy.md">Privacy Policy</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://docs.onesoftnet.eu.org/">Documentation</a></li>
+                </ul>
+            </div>
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,22 @@ import { githubRepo } from '../utils/links';
 
 export default function Home() {
     return (
-        <div className={styles.wrapper}>
-            <h1 className='text-2xl md:text-4xl lg:text-5xl text-center md:px-[25%] !leading-[2.5ch] mt-3'>A single bot for <span className="text-blue-500">everything you need</span> to <span className="text-blue-500">moderate</span> a Discord Server.</h1>
-            <h3 className="text-center text-xl md:text-2xl lg:text-3xl mt-5">It's <span className='text-blue-500'>SudoBot</span>. The ultimate solution for Discord Server Moderation.</h3>
+        <div>
+            <div className={styles.wrapper}>
+                <h1 className='text-2xl md:text-4xl lg:text-5xl text-center md:px-[25%] !leading-[2.5ch] mt-3'>A single bot for <span className="text-blue-500">everything you need</span> to <span className="text-blue-500">moderate</span> a Discord Server.</h1>
+                <h3 className="text-center text-xl md:text-2xl lg:text-3xl mt-5">It's <span className='text-blue-500'>SudoBot</span>. The ultimate solution for Discord Server Moderation.</h3>
 
-            <br />
-            <br />
-        
-            <div className='text-center mx-5 ml-0'>
-                <ThemeButton className='px-3 md:px-5 py-2 md:py-3 w-[100%] md:w-auto block md:inline-block mx-3 mb-3 md:mb-0' size='large' variant='outlined'>Get SudoBot</ThemeButton>
-                <a href={`${githubRepo}/releases`} target="_blank" rel='noreferrer'><ThemeButton className='px-3 md:px-5 py-2 md:py-3 w-[100%] md:w-auto block md:inline-block mx-3' size='large' variant='outlined'>Download Latest Release</ThemeButton></a>
+                <br />
+                <br />
+            
+                <div className='text-center mx-5 ml-0'>
+                    <ThemeButton className='px-3 md:px-5 py-2 md:py-3 w-[100%] md:w-auto block md:inline-block mx-3 mb-3 md:mb-0' size='large' variant='outlined'>Get SudoBot</ThemeButton>
+                    <a href={`${githubRepo}/releases/latest`} target="_blank" rel='noreferrer'><ThemeButton className='px-3 md:px-5 py-2 md:py-3 w-[100%] md:w-auto block md:inline-block mx-3' size='large' variant='outlined'>Download Latest Release</ThemeButton></a>
+                </div>
+            </div>
+                        
+            <div style={{ padding: 50 }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cumque, fugiat necessitatibus ipsam ad in quam alias magnam nam ipsum deserunt officia perferendis possimus rem sint id incidunt exercitationem officiis.
             </div>
         </div>
     );
