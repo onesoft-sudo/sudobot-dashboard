64368b0e761e537dbad08735f8ef444c460f16d7
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2023-08-09 01:09:35 +0600

    fix: trying to use client side hook in a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import Footer from "@/components/Common/Footer";
-import useAuthWithCheck from "@/hooks/useAuthWithCheck";
 import Top from "../components/Common/Top";
 import FeatureOverview from "../components/Home/FeatureOverview";
 
 export default function Home() {
-    const { user } = useAuthWithCheck();
-
-    if (user === undefined) {
-        return <div></div>;
-    }
-
     return (
         <>
             <main>
