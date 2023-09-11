566f16e8dad4c15689ab45ea591812455f562381
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-09-11 20:57:33 +0600

    fix: select menu description in permission mode card

--- src/components/SettingCards/PermissionModeCard.tsx
+++ src/components/SettingCards/PermissionModeCard.tsx
@@ -7,24 +7,29 @@ import {
     SelectItem,
 } from "@nextui-org/react";
 import { FC, useState } from "react";
+import { FaDiscord } from "react-icons/fa6";
+import { HiShieldCheck, HiSquare2Stack } from "react-icons/hi2";
 
 const permissionModes = [
     {
         name: "Discord",
-        description: "Completely rely on Discord-based permissions",
+        description: "Completely rely on Discord-based permissions.",
         id: "discord",
+        icon: FaDiscord,
     },
     {
         name: "Level-based",
         description:
-            "Create and manage bot permissions using different permission levels represented by numbers 0-100",
+            "Create and manage bot permissions using different permission levels represented by numbers 0-100.",
         id: "levels",
+        icon: HiSquare2Stack,
     },
     {
-        name: "Permission Role (Advanced)",
+        name: "Permission Roles (Advanced)",
         description:
-            "Create and manage bot permissions using different permission levels represented by numbers 0-100",
+            "Create and manage custom bot permission roles that specify who will receive what permissions and much more.",
         id: "advanced",
+        icon: HiShieldCheck,
     },
 ];
 
@@ -64,14 +69,21 @@ const PermissionModeCard: FC<SettingCardProps> = ({
                         value: mode,
                     })}
                     selectedKeys={[mode]}
+                    classNames={{
+                        popover: "[overflow-wrap:break-word]",
+                        innerWrapper: "[overflow-wrap:break-word]",
+                        value: "[overflow-wrap:break-word]",
+                        description: "[overflow-wrap:break-word]",
+                    }}
                 >
                     {permissionModes.map(permissionMode => (
                         <SelectItem
                             key={permissionMode.id}
-                            textValue={permissionMode.name}
-                        >
-                            <div>{permissionMode.name}</div>
-                        </SelectItem>
+                            title={permissionMode.name}
+                            className="relative"
+                            description={permissionMode.description}
+                            startContent={<permissionMode.icon size={25} />}
+                        />
                     ))}
                 </Select>
             </CardBody>
