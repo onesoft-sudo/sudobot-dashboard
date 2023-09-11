967e15532cebe37e63b651e78b8cf5acaf6afd46
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-09-11 17:18:47 +0600

    feat(seo): disallow robots to index or follow /dashboard URLs

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,14 @@
 
 import useAuthWithCheck from "@/hooks/useAuthWithCheck";
 import { FC, PropsWithChildren } from "react";
+import { Metadata } from "next";
+
+const metadata: Metadata = {
+    robots: {
+        follow: false,
+        index: false,
+    }
+};
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
     const { user } = useAuthWithCheck();
