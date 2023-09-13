c4e5e446fa2377379d0c3c8fbf6d0b0227a295f1
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-09-13 21:33:13 +0600

    fix: sidebar styles not being applied in some cases on mobile devices

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -3,9 +3,9 @@
 import { useAuthContext } from "@/contexts/AuthContext";
 import useLoggedIn from "@/hooks/useLoggedIn";
 import { sidebarItems } from "@/utils/links";
+import { Box } from "@mui/material";
 import { usePathname } from "next/navigation";
 import { FC, Fragment } from "react";
-import styles from "../../styles/Sidebar.module.css";
 import Link from "../Router/Link";
 
 const Sidebar: FC = () => {
@@ -18,11 +18,23 @@ const Sidebar: FC = () => {
 
     return (
         <aside className={`h-[100%]`}>
-            <div className={styles.items}>
+            <Box
+                className="scrollbar-hide md:scrollbar-default bg-[linear-gradient(45deg,rgba(255,255,255,0.05),rgba(255,255,255,0.1))] h-[100%] relative shadow-[0_0_5px_0_rgba(255,255,255,0.3)] [backdrop-filter:blur(10px)] md:h-[calc(100vh-65px)] md:mb-0"
+                sx={{
+                    "@media (max-width: 970px)": {
+                        scrollbarWidth: "none",
+                        msOverflowStyle: "none",
+                        height: "auto",
+                        display: "flex",
+                        alignItems: "center",
+                        overflowX: "scroll",
+                    },
+                }}
+            >
                 {sidebarItems.map((item, index) => (
                     <Fragment key={index}>
                         {index === sidebarItems.length - 1 && (
-                            <div className={styles.spacer}></div>
+                            <div className="hidden md:absolute md:block bottom-[46px] w-[100%] bg-[#333] h-[1px] rounded-[10px]"></div>
                         )}
                         <Link
                             href={item.url.replaceAll(
@@ -30,22 +42,22 @@ const Sidebar: FC = () => {
                                 currentGuild?.id ?? "__"
                             )}
                             className={
-                                styles.item +
+                                "md:my-[5px] flex items-center gap-[15px] [font-size:1.04em] py-[10px] px-[20px] md:last:absolute md:last:bottom-0 md:last:w-[100%] md:last:my-0 md:last:py-[10px] [min-width:max-content] md:[min-width:auto] mt-0 md:mt-[inherit] hover:bg-[rgba(255,255,255,0.15)]" +
                                 (pathname ===
                                 item.url.replaceAll(
                                     "{id}",
                                     currentGuild?.id ?? "__"
                                 )
-                                    ? ` ${styles.activeItem}`
+                                    ? " md:[border-left:1px_solid_#007bff] relative after:content-[''] after:absolute after:bottom-[0] after:left-0 after:w-[100%] after:bg-[#007bff] after:h-[3px] after:rounded-[10px] after:hover:rounded-tl-[inherit] after:rounded-b-[0] md:after:hidden rounded-tl-none rounded-bl-none"
                                     : "")
                             }
                         >
-                            <item.icon className={styles.icon} />
+                            <item.icon />
                             <span>{item.name}</span>
                         </Link>
                     </Fragment>
                 ))}
-            </div>
+            </Box>
         </aside>
     );
 };
