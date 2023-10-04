e76ecb464fbec7739b236335f02b19c7410d91a8
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-10-04 12:18:01 +0600

    fix: auto logout if the token expires

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,21 +1,21 @@
 /*
-* This file is part of SudoBot Dashboard.
-*
-* Copyright (C) 2021-2023 OSN Developers.
-*
-* SudoBot Dashboard is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SudoBot Dashboard is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with SudoBot Dashboard. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of SudoBot Dashboard.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot Dashboard is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot Dashboard is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot Dashboard. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 "use client";
 
@@ -192,7 +192,7 @@ export function AuthContextProvider({ children }: PropsWithChildren) {
             if ("tokenExpiresAt" in user) {
                 const tokenExpiresAt = new Date(user.tokenExpiresAt);
 
-                if (tokenExpiresAt.getTime() <= Date.now()) {
+                if (Date.now() > tokenExpiresAt.getTime()) {
                     console.log("Token expired");
 
                     dispatch({
