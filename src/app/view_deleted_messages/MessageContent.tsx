d344d6fb6b5432862593eebb5a754057568a0d52
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-11-05 19:39:59 +0600

    fix(deleted_messages): text overflow

--- src/app/view_deleted_messages/MessageContent.tsx
+++ src/app/view_deleted_messages/MessageContent.tsx
@@ -1,21 +1,21 @@
 /*
-* This file is part of SudoBot Dashboard.
-*
-* Copyright (C) 2021-2023 OSN Developers.
-*
-* SudoBot Dashboard is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SudoBot Dashboard is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with SudoBot Dashboard. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of SudoBot Dashboard.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot Dashboard is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot Dashboard is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot Dashboard. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 "use client";
 
@@ -39,7 +39,10 @@ const MessageContent: FC<MessageContentProps> = ({
 }) => {
     return (
         <Markdown>
-            <ReactMarkdown rehypePlugins={[remarkGfm as any, remarkBreaks]}>
+            <ReactMarkdown
+                rehypePlugins={[remarkGfm as any, remarkBreaks]}
+                className="[overflow-wrap:anywhere] relative"
+            >
                 {content
                     .replace(/<@(!)?\d+>/gim, matchedString => {
                         const id = matchedString.substring(
--- src/app/view_deleted_messages/invalid.tsx
+++ src/app/view_deleted_messages/invalid.tsx
@@ -1,21 +1,21 @@
 /*
-* This file is part of SudoBot Dashboard.
-*
-* Copyright (C) 2021-2023 OSN Developers.
-*
-* SudoBot Dashboard is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SudoBot Dashboard is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with SudoBot Dashboard. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of SudoBot Dashboard.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot Dashboard is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot Dashboard is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot Dashboard. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import { Button } from "@mui/material";
 import { FC } from "react";
@@ -24,11 +24,14 @@ import { MdError } from "react-icons/md";
 interface InvalidProps {
     url?: string | string[];
     error?: boolean;
+    as?: "div" | "main";
 }
 
-const Invalid: FC<InvalidProps> = ({ url, error }) => {
+const Invalid: FC<InvalidProps> = ({ url, error, as = "main" }) => {
+    const Component = as;
+
     return (
-        <main className="flex justify-center items-center flex-col my-5 gap-3 text-red-600">
+        <Component className="flex justify-center items-center flex-col my-5 gap-3 text-red-600">
             <MdError size={40} />
             <div className="text-center text-2xl md:text-3xl">
                 {!error ? (
@@ -45,7 +48,7 @@ const Invalid: FC<InvalidProps> = ({ url, error }) => {
                     Return to Home
                 </Button>
             </div>
-        </main>
+        </Component>
     );
 };
 
