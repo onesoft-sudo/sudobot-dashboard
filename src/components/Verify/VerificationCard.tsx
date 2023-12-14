ca20a305b0415e079bb98d6ec4bd5435f93864a9
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-12-14 22:46:50 +0600

    feat: add verification card component

--- src/components/Verify/VerificationCard.tsx
+++ src/components/Verify/VerificationCard.tsx
@@ -1,8 +1,15 @@
-import { PropsWithChildren } from "react";
+import { ComponentProps } from "react";
 
-const Card = ({ children }: PropsWithChildren) => {
+const Card = ({
+    children,
+    className = "",
+    ...props
+}: ComponentProps<"div">) => {
     return (
-        <div className="md:mx-0 max-w-[90vw] w-[90vw] md:w-[40vw] lg:w-[25vw] xl:w-[17vw] shadow-[0_0_2px_0_rgba(255,255,255,0.5)] rounded-[10px]">
+        <div
+            {...props}
+            className={`md:mx-0 max-w-[90vw] w-[90vw] md:w-[40vw] lg:w-[25vw] xl:w-[17vw] shadow-[0_0_2px_0_rgba(255,255,255,0.5)] rounded-[10px] ${className}`}
+        >
             {children}
         </div>
     );
