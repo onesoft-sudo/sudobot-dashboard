c7aee97fb73be6b505667b6423ae3c480bfe880b
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-05-06 21:25:31 +0600

    chore: setup commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,29 @@
+import {type UserConfig,RuleConfigSeverity} from '@commitlint/types';
+
+const configuration: UserConfig = { 
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            RuleConfigSeverity.Error,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "refactor",
+                "perf",
+                "test",
+                "build",
+                "ci",
+                "chore",
+                "revert",
+                "wip",
+                "release",
+                "deploy"
+            ]
+        ]
+    }
+};
+
+export default configuration;
