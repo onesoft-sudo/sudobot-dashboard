5891041d5f9c34b7320d83031ae380a80e7db3f0
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-06-07 18:09:39 +0000

    fix: nextjs build error for not using suspense boundary

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -1,5 +1,6 @@
 import LoginForm from "@/components/Login/LoginForm";
 import { Box } from "@mui/material";
+import { Suspense } from "react";
 
 export default function LoginPage() {
     return (
@@ -8,7 +9,9 @@ export default function LoginPage() {
 
             <div className="relative mt-5 flex min-h-full w-full items-center justify-center">
                 <Box className="mx-5 w-full rounded-lg bg-[linear-gradient(rgba(255,255,255,0.3),rgba(255,255,255,0.3))] p-3 shadow-[0_0_2px_0_rgba(0,0,0,0.17)] dark:bg-[linear-gradient(to_right,rgba(45,45,45,0.5),rgba(45,45,45,0.6))] dark:shadow-[0_0_2px_0_rgba(255,255,255,0.6)] sm:mx-0 sm:w-80 md:w-96">
-                    <LoginForm />
+                    <Suspense fallback={null}>
+                        <LoginForm />
+                    </Suspense>
                 </Box>
             </div>
         </div>
