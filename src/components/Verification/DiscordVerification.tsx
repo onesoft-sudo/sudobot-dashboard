d3c4898a0abe4ccb6b2dcc6c2f403c5b4d9fad7a
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-07-06 10:32:29 +0000

    fix: eslint warnings

--- src/components/Verification/DiscordVerification.tsx
+++ src/components/Verification/DiscordVerification.tsx
@@ -24,7 +24,7 @@ const DiscordVerification: FC<DiscordVerificationProps> = ({ code, token }) => {
     useEffect(() => mutation.mutate(), []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
-        <div className="flex flex-col items-center justify-center rounded-lg bg-white/70 p-3 shadow-[0_0_2px_0_rgba(0,0,0,0.2)] sm:w-80 md:w-96 dark:shadow-[0_0_2px_0_rgba(255,255,255,0.6)] dark:[background:linear-gradient(to_right,rgba(45,45,45,0.5),rgba(45,45,45,0.6))]">
+        <div className="flex flex-col items-center justify-center rounded-lg bg-white/70 p-3 shadow-[0_0_2px_0_rgba(0,0,0,0.2)] dark:shadow-[0_0_2px_0_rgba(255,255,255,0.6)] dark:[background:linear-gradient(to_right,rgba(45,45,45,0.5),rgba(45,45,45,0.6))] sm:w-80 md:w-96">
             {mutation.isPending && <CircularProgress />}
             {mutation.isSuccess && <MdCheck className="text-green-500" size="2.5rem" />}
             {mutation.isError && <MdError className="text-red-500" size="2.5rem" />}
