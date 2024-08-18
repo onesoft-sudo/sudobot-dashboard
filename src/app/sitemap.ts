b5c13fb6db61d8a8b216b7e81465a927337b3de3
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-08-18 09:07:38 +0600

    fix(seo): update sitemap.ts to have the new URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
  
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseURL = process.env.NEXT_PUBLIC_BASE_URL ?? "https://www.sudobot.org";
+    const baseURL = process.env.NEXT_PUBLIC_BASE_URL ?? "https://sudobot.onesoftnet.eu.org";
  
     return ['/', '/login'].map(route => ({
         url: `${baseURL}${route}`,
