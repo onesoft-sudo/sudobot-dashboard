b58d6ddf8342f0b61e29d810c126d83bcbb773b1
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-08-24 07:54:53 +0000

    fix(page:home): better and more attractive cards
    Signed-off-by: GitHub <noreply@github.com>

--- src/components/Cards/FeatureCard.tsx
+++ src/components/Cards/FeatureCard.tsx
@@ -10,7 +10,7 @@ const FeatureCard: FC<FeatureCardProps> = ({ children, className }) => {
     return (
         <div
             className={twMerge(
-                "overflow-hidden bg-gradient-to-tr from-[rgba(0,123,255,0.1)] to-[rgba(0,123,255,0.2)] rounded shadow-[0_0_4px_1px_rgba(0,0,0,0.1)] dark:shadow-[0_0_1px_1px_rgba(0,123,255,0.3)] hover:outline hover:outline-2 hover:outline-gray-200 dark:hover:outline-gray-700 focus:outline focus:outline-2 focus:outline-blue-600 dark:focus:outline-blue-600",
+                "border-1 border-blue-500/50 dark:border-blue-800/50 overflow-hidden bg-gradient-to-tr from-[rgba(0,123,255,0.1)] to-[rgba(0,123,255,0.2)] rounded shadow-[0_0_4px_1px_rgba(0,0,0,0.1)] dark:shadow-[0_0_1px_1px_rgba(0,123,255,0.3)] hover:outline hover:outline-2 hover:outline-gray-200 dark:hover:outline-gray-700 focus:outline focus:outline-2 focus:outline-blue-600 dark:focus:outline-blue-600",
                 className,
             )}
             tabIndex={-1}
